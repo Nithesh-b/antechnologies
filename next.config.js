5d21c90ac0a83f1dfda86cd0e7ca870016935f4c
Nithesh Balakrishnan <antechnologiesindia@gmail.com>
2025-01-07 22:43:38 +0530

    Updated changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Creates a static export
-  basePath: process.env.NODE_ENV === 'production' ? '/antechnologies/' : '', // Replace with your repo name
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/antechnologies' : '',
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
   },
 }
 
